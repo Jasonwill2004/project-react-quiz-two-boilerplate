84bfaf5b8f099928abd8d71bf5566359d6d286c4
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-06 16:36:29 +0530

    done

---
+++ quiz/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+// import HomeComponent from './components/HomeComponents';
+import QuizComponent from './components/QuizComponent';
+import ResultComponent from './components/ResultComponent';
+// import QuizComponent from './components/QuizComponent';
+// import ResultComponent from './components/ResultComponents';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <HomeComponent/> */}
+      <QuizComponent/>
+      {/* <ResultComponent/> */}
+      
+    </div>
+  );
+}
+
+export default App;
